cae6f36f4f032939a0d736a752b1354a4d0cccb1
Pjotor87 <pjotor87@hotmail.com>
2015-01-28 20:22:50 +0100

    Initial commit

---
+++ RaceForTheGalaxy/RaceForTheGalaxy/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace RaceForTheGalaxy
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
